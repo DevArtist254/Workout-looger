35dd37cade7dd67c13ade3906f0cff4a3948b2e2
DevArtist254 <kevinwasonga415@outlook.com>
2023-08-20 14:58:53 +0300

    sidelogger

--- script.js
+++ script.js
@@ -1,13 +1,81 @@
+class Workout {
+  id = Math.floor(Math.random() * 1000);
+  date = new Date().getDate();
+  month = new Date().getMonth();
+  hours = new Date().getHours();
+  minutes = new Date().getMinutes();
+
+  constructor(distance, duration) {
+    this.distance = distance;
+    this.duration = duration;
+  }
+
+  description() {
+    const months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+
+    return `on ${this.date}/${months[this.month]} at ${this.hours}:${
+      this.minutes
+    }`;
+  }
+
+  speed() {
+    return this.distance / this.duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(distance, duration, typed, elevation) {
+    super(distance, duration);
+    this.typed = typed;
+    this.elevation = elevation;
+    this.description();
+    this.speed();
+  }
+}
+
+class Cycling extends Workout {
+  constructor(distance, duration, typed, cadence) {
+    super(distance, duration);
+    this.typed = typed;
+    this.cadence = cadence;
+    this.description();
+    this.speed();
+  }
+}
+
 const workoutSelector = document.getElementById('type-selecting');
 const cadenceArea = document.querySelector('.cadence');
 const elevArea = document.querySelector('.elevation');
+const workoutsArea = document.querySelector('.workouts');
+
+const form = document.getElementById('form');
+const durationInput = document.getElementById('durationInput');
+const distanceInput = document.getElementById('distanceInput');
+const cadenceInput = document.getElementById('cadenceInput');
+const elevationInput = document.getElementById('elevationInput');
 
 class App {
+  #selectedWorkout = 'cycling';
   #map;
   #currentCoords;
   #zoomLevel = 13;
+  #workouts = [];
   constructor() {
     this._getCurrentLocation();
+
+    form.addEventListener('submit', this._workoutLogger.bind(this));
 
     workoutSelector.addEventListener(
       'change',
@@ -41,9 +109,99 @@ class App {
     // }).addTo(this.#map);
   }
 
+  _workoutLogger(e) {
+    function numberChecker(numbers) {
+      return numbers.every((number) => Number.isFinite(number));
+    }
+
+    e.preventDefault();
+
+    const distance = +distanceInput.value;
+    const duration = +durationInput.value;
+    let workout;
+
+    if (this.#selectedWorkout === 'cycling') {
+      const elev = 3;
+      if (!numberChecker([distance, duration, elev]))
+        return alert('Please enter a number!');
+
+      workout = new Cycling(distance, duration, this.#selectedWorkout, elev);
+    }
+
+    if (this.#selectedWorkout === 'running') {
+      const cadence = 5;
+
+      if (!numberChecker([distance, duration, cadence]))
+        return alert('Please enter a number!');
+
+      workout = new Running(distance, duration, this.#selectedWorkout, cadence);
+    }
+
+    this.#workouts.push(workout);
+
+    this._renderWorkout(workout);
+  }
+
   _toggleBtwnRunningCycling(e) {
-    cadenceArea.classList.toggle('cadenceHidden');
-    elevArea.classList.toggle('elevHidden');
+    this.#selectedWorkout = e.target.value;
+
+    if (this.#selectedWorkout === 'cycling') {
+      cadenceArea.classList.add('cadenceHidden');
+      elevArea.classList.remove('elevHidden');
+    } else if (this.#selectedWorkout === 'running') {
+      cadenceArea.classList.remove('cadenceHidden');
+      elevArea.classList.add('elevHidden');
+    }
+  }
+
+  _renderWorkout(workout) {
+    let html;
+
+    if (workout.typed === 'running') {
+      html = `
+    <div class="workout bdr-${workout.typed}">
+            <div class="workout__title">${
+              workout.typed
+            } on ${workout.description()}</div>
+            <div class="workout__details">
+              <div class="workout__details--distance">${
+                workout.typed === 'cycling' ? `🚴‍♀️` : `🏃‍♂️`
+              } ${workout.distance} KM</div>
+              <div class="workout__details--duration">🦶🏼 ${
+                workout.duration
+              } MIN</div>
+              <div class="workout__details--speed">⚡️ ${workout.speed()} KM/H</div>
+              <div class="workout__details--cadence">⛰ ${
+                workout.cadence
+              } M</div>
+            </div>
+          </div>
+    `;
+    }
+
+    if (workout.typed === 'cycling') {
+      html = `
+    <div class="workout bdr-${workout.typed}">
+            <div class="workout__title">${
+              workout.typed
+            } on ${workout.description()}</div>
+            <div class="workout__details">
+              <div class="workout__details--distance">${
+                workout.typed === 'cycling' ? `🚴‍♀️` : `🏃‍♂️`
+              } ${workout.distance} KM</div>
+              <div class="workout__details--duration">🦶🏼 ${
+                workout.duration
+              } MIN</div>
+              <div class="workout__details--speed">⚡️ ${workout.speed()} KM/H</div>
+              <div class="workout__details--cadence">⛰ ${
+                workout.elevation
+              } M</div>
+            </div>
+          </div>
+    `;
+    }
+
+    workoutsArea.insertAdjacentHTML('beforeend', html);
   }
 }
 
