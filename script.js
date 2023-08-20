f684015c83acd1a0dd2298cce1d4c2d89395345a
DevArtist254 <kevinwasonga415@outlook.com>
2023-08-20 10:31:16 +0300

    _toggleBtwnRunningCycling

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+const workoutSelector = document.getElementById('type-selecting');
+const cadenceArea = document.querySelector('.cadence');
+const elevArea = document.querySelector('.elevation');
+
 class App {
   #map;
   #currentCoords;
   #zoomLevel = 13;
   constructor() {
     this._getCurrentLocation();
+
+    workoutSelector.addEventListener(
+      'change',
+      this._toggleBtwnRunningCycling.bind(this)
+    );
   }
   _getCurrentLocation() {
     navigator.geolocation.getCurrentPosition(
@@ -24,12 +33,17 @@ class App {
 
     const coords = [lat, lng];
 
-    this.#map = L.map('map').setView(coords, this.#zoomLevel);
+    // this.#map = L.map('map').setView(coords, this.#zoomLevel);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(this.#map);
+    // L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    //   attribution:
+    //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    // }).addTo(this.#map);
+  }
+
+  _toggleBtwnRunningCycling(e) {
+    cadenceArea.classList.toggle('cadenceHidden');
+    elevArea.classList.toggle('elevHidden');
   }
 }
 
